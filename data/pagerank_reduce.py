bd3cbad0a184ee252e8a5c6b2c68d43a991eb108
Bryant Lin <bjlin@caltech.edu>
2016-02-09 04:43:27 -0800

    A number of bug fixes, comments; initial reduce.

--- data/pagerank_reduce.py
+++ data/pagerank_reduce.py
@@ -1,11 +1,69 @@
 #!/usr/bin/env python
 
 import sys
+import re
 
-#
-# This program simply represents the identity function.
-#
+ALPHA = 0.85
+find_tabs = re.compile('\t')
+find_colons = re.compile(':')
+
+def format_line(line):
+    key, tmp_values = find_tabs.split(line.strip())
+    value_type, value = find_colons.split(tmp_values)
+    if value_type == 'Adjacencies':
+        return (key, value_type, value)
+    elif value_type == 'PrevPageRank':
+        return (key, value_type, float(value))
+    elif value_type == 'Value':
+        return (key, value_type, float(value))
+    else:
+        raise Exception('Not a valid type.')
+
+
+def is_final_rank(line):
+    ''' Checks whether line starts with 'FinalRank:'. '''
+    return len(line) > 10 and line[0:10] == 'FinalRank:'
+
+def print_output(node, rank, prev, adjacencies):
+    comma = ','
+    if adjacencies == '':
+        comma = ''
+    sys.stdout.write('NodeId:%s\t%f,%f%s%s\n' %
+            (node, rank, prev, comma, adjacencies))
+
+curr_node = ''
+curr_sum = 0
+curr_count = 0
+curr_prev = 0
+curr_adjacencies = ''
 
 for line in sys.stdin:
-    sys.stdout.write(line)
+    if is_final_rank(line):
+        sys.stdout.write(line)
+        continue
 
+    node, value_type, value = format_line(line)
+    if node != curr_node:
+        # print out info on previous node
+        if curr_node != '':
+            print_output(curr_node, ALPHA * curr_sum + 1-ALPHA,
+                    curr_prev, curr_adjacencies)
+        # reset values
+        curr_node = node
+        curr_sum = 0
+        curr_count = 0
+        curr_prev = 0
+        curr_adjacencies = ''
+
+    if value_type == 'Adjacencies':
+        curr_adjacencies = value
+    elif value_type == 'PrevPageRank':
+        curr_prev = value
+    elif value_type == 'Value':
+        curr_sum += value
+        curr_count += 1
+
+
+if curr_node != '':
+    print_output(curr_node, ALPHA * curr_sum + 1-ALPHA,
+            curr_prev, curr_adjacencies)

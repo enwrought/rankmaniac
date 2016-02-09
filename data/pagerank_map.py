329c9c6dc3a91042325c885bdb6287e57d4a2384
Bryant Lin <bjlin@caltech.edu>
2016-02-09 04:43:27 -0800

    A number of bug fixes, comments; initial reduce.

--- data/pagerank_map.py
+++ data/pagerank_map.py
@@ -3,10 +3,6 @@
 import sys
 import re
 
-#
-# This program simply represents the identity function.
-#
-
 find_tabs = re.compile('\t')
 find_commas = re.compile(',')
 
@@ -16,9 +12,28 @@ def format_line(line):
     values = find_commas.split(tmp_values)
     return (key, float(values[0]), float(values[1]), values[2:])
 
+def is_final_rank(line):
+    ''' Checks whether line starts with 'FinalRank:'. '''
+    return len(line) > 10 and line[0:10] == 'FinalRank:'
+
+def is_in_stopping_criteria(curr, prev):
+    ''' Checks whether the stopping criteria is satisfies given current
+        and previous PageRank values.'''
+    return (curr - prev) ** 2 < 0.01
+
 for line in sys.stdin:
+    if is_final_rank(line):
+        sys.stdout.write(line)
+        continue
+
     node, curr, prev, adjacencies = format_line(line)
     n = len(adjacencies)
+
+    if is_in_stopping_criteria(curr, prev):
+        sys.stdout.write('FinalRank:%f\t%s\n' % (curr, node))
+        continue
+
+    # Otherwise, we need to continue iterating for node
 
     # Also print out curr (for new value of prev) and adjacencies
     sys.stdout.write('%s\tAdjacencies:%s\n' % (node, ','.join(adjacencies)))

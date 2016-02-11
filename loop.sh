1a6d114c547c1891e67baf728be7128783e48a36
matathias <warrick.david58@gmail.com>
2016-02-10 19:44:27 -0800

    Created script to help test code locally.
    Script is called 'loop.sh'. Open to change number of iterations as necessary. Will create as many files as iterations.

---
+++ loop.sh
@@ -0,0 +1,9 @@
+python data/pagerank_map.py < local_test_data/GNPn100p05 | sort | python data/pagerank_reduce.py | python data/process_map.py | sort | python data/process_reduce.py > output0.txt
+
+for i in `seq 1 10`; do
+    IN=output$(($i-1)).txt
+    OUT=output$(($i)).txt
+    echo $IN
+    echo $OUT
+    python data/pagerank_map.py < $IN | sort | python data/pagerank_reduce.py | python data/process_map.py | sort | python data/process_reduce.py > $OUT
+done
